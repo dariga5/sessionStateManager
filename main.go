af754d94396f9ec6dbac1542cf1ed1864713fb29
Dariga5 <isa2407@mail.ru>
2023-11-27 23:54:28 +0500

    dev

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"SSM/main/session_s"
-)
-
 func main() {
 
-	var s = session_s.CreateSessionObj()
-
-	var err = s.GetCache().GetData(3).Err
-
-	s.GetCache().PullRequest("1")
-	s.GetCache().PullRequest("2")
-	s.GetCache().PullRequest("3")
-	s.GetCache().PullRequest("4")
-	s.GetCache().PullRequest("5")
-	s.GetCache().PullRequest("6")
-	s.GetCache().PullRequest("7")
-	s.GetCache().PullRequest("8")
-	s.GetCache().PullRequest("9")
-	var err1 = s.GetCache().PullRequest("10")
-
-	if err1 != nil || err != nil {
-
-		print(err, err1)
-	}
 }
