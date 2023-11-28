3622da017c44ea45fec02c73df1168dd122da65d
i_astanin <i_astanin@orbank.ru>
2023-11-28 17:11:31 +0500

    dev

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import "SSM/main/session_s"
 func main() {
 	var s = session_s.CreateSessionObj()
 
-	print(s.GetCache().PushRequest("1"))
-	print(s.GetCache().PushRequest("2"))
-	print(s.GetCache().PushRequest("3"))
-	print(s.GetCache().PushRequest("4"))
-	print(s.GetCache().PushRequest("5"))
-	print(s.GetCache().PushRequest("6"))
-	print(s.GetCache().PushRequest("7"))
-	print(s.GetCache().PushRequest("8"))
-	print(s.GetCache().PushRequest("9"))
-	print(s.GetCache().PushRequest("0"))
+	print(s.GetCache().PushRequest("1").Err)
+	print(s.GetCache().PushRequest("2").Err)
+	print(s.GetCache().PushRequest("3").Err)
+	print(s.GetCache().PushRequest("4").Err)
+	print(s.GetCache().PushRequest("5").Err)
+	print(s.GetCache().PushRequest("6").Err)
+	print(s.GetCache().PushRequest("7").Err)
+	print(s.GetCache().PushRequest("8").Err)
+	print(s.GetCache().PushRequest("9").Err)
+	print(s.GetCache().PushRequest("0").Err)
 
 }
